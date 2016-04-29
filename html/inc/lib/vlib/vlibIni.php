6253136783cc5bb46a80498be05a5c5d38b2d31a
ReneMuetti <ReneMuetti@googlemail.com>
2016-04-29 10:12:30 +0200

    Update vlibIni.php
    Fixed PHP-Error

--- html/inc/lib/vlib/vlibIni.php
+++ html/inc/lib/vlib/vlibIni.php
@@ -43,7 +43,7 @@ if (!defined('vlibIniClassLoaded')) {
     class vlibIni {
 
         /** config vars for vlibTemplate */
-        function vlibTemplate () {
+        public static function vlibTemplate () {
             global $cfg;
 
             $cache_dir = $cfg["path"].'.templateCache';
